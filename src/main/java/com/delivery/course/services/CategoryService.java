0fc2a0a68aa4cfc19e1db09713c14d501059953f
Paulo Bitencourt <paulo86roberto@gmail.com>
2020-09-29 18:19:44 -0300

    Category

---
+++ src/main/java/com/delivery/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.delivery.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.delivery.course.entities.Category;
+import com.delivery.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
