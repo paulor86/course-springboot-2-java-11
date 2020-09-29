a2caa844ed317d989ebced76b4720283f9f4e4da
Paulo Bitencourt <paulo86roberto@gmail.com>
2020-09-29 16:10:10 -0300

    User entity and resource

---
+++ src/main/java/com/delivery/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.delivery.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.delivery.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Paulo Bitencourt", "paulo86roberto@gmail.com", "+5592994073579", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+}
