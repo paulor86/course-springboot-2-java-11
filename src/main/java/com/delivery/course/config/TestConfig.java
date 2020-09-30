f2a8c0fe5f4f15d7fd3a716ce4cd4dcd02452bf5
Paulo Bitencourt <paulo86roberto@gmail.com>
2020-09-29 22:29:07 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/delivery/course/config/TestConfig.java
+++ src/main/java/com/delivery/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.delivery.course.entities.Category;
 import com.delivery.course.entities.Order;
+import com.delivery.course.entities.OrderItem;
 import com.delivery.course.entities.Product;
 import com.delivery.course.entities.User;
 import com.delivery.course.entities.enums.OrderStatus;
 import com.delivery.course.repositories.CategoryRepository;
+import com.delivery.course.repositories.OrderItemRepository;
 import com.delivery.course.repositories.OrderRepository;
 import com.delivery.course.repositories.ProductRepository;
 import com.delivery.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
---
+++ src/main/java/com/delivery/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.delivery.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.delivery.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
